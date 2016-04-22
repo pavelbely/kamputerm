6b0a23e8395a9e5b48431587342302e6ef60d995
vaukalak <lahmataja.pa4vara@gmia>
2016-04-22 15:03:28 +0300

    rolled back tests

--- server/test/word/model/DefinitionModel.test.js
+++ server/test/word/model/DefinitionModel.test.js
@@ -1,124 +1,42 @@
 'use strict';
 
-var _regenerator = require('babel-runtime/regenerator');
-
-var _regenerator2 = _interopRequireDefault(_regenerator);
-
-var _assert = require('assert');
-
-var _assert2 = _interopRequireDefault(_assert);
-
-var _chai = require('chai');
-
-var _coMocha = require('co-mocha');
-
-var _coMocha2 = _interopRequireDefault(_coMocha);
-
-var _DefinitionModel = require('../../../src/definition/model/DefinitionModel.js');
-
-var _stub = require('../stubs/stub.js');
-
-var stubs = _interopRequireWildcard(_stub);
-
-function _interopRequireWildcard(obj) {
-    if (obj && obj.__esModule) {
-        return obj;
-    } else {
-        var newObj = {};if (obj != null) {
-            for (var key in obj) {
-                if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key];
-            }
-        }newObj.default = obj;return newObj;
-    }
-}
-
-function _interopRequireDefault(obj) {
-    return obj && obj.__esModule ? obj : { default: obj };
-}
+import assert from 'assert';
+import {expect} from 'chai';
+import coMocha from 'co-mocha';
+import { DefinitionModel } from '../../../src/definition/model/DefinitionModel.js';
+import * as stubs from '../stubs/stub.js';
 
 describe('Definition Model', function () {
 
     beforeEach(function () {
-        this._testData = { source: stubs.createStubWord() };
+        this._testData = {source: stubs.createStubWord()};
     });
 
     describe('validation', function () {
-        it('should validate correct object', _regenerator2.default.mark(function _callee() {
-            return _regenerator2.default.wrap(function _callee$(_context) {
-                while (1) {
-                    switch (_context.prev = _context.next) {
-                        case 0:
-                            _context.next = 2;
-                            return _DefinitionModel.DefinitionModel.checkValidity(this._testData.source);
+        it('should validate correct object', function* () {
+            yield DefinitionModel.checkValidity(this._testData.source);
+        });
 
-                        case 2:
-                        case 'end':
-                            return _context.stop();
-                    }
-                }
-            }, _callee, this);
-        }));
+        it('should fail with invalid tags', function* () {
+            try {
+                this._testData.source.meta.tags.push({});
+                yield DefinitionModel.checkValidity(this._testData.source);
+                assert.fail('should fail');
+            } catch (err) {
+                expect(err.errors).to.have.property('meta.tags');
+                expect(err.name).to.equal('ValidationError');
+            }
+        });
 
-        it('should fail with invalid tags', _regenerator2.default.mark(function _callee2() {
-            return _regenerator2.default.wrap(function _callee2$(_context2) {
-                while (1) {
-                    switch (_context2.prev = _context2.next) {
-                        case 0:
-                            _context2.prev = 0;
-
-                            this._testData.source.meta.tags.push({});
-                            _context2.next = 4;
-                            return _DefinitionModel.DefinitionModel.checkValidity(this._testData.source);
-
-                        case 4:
-                            _assert2.default.fail('should fail');
-                            _context2.next = 11;
-                            break;
-
-                        case 7:
-                            _context2.prev = 7;
-                            _context2.t0 = _context2['catch'](0);
-
-                            (0, _chai.expect)(_context2.t0.errors).to.have.property('meta.tags');
-                            (0, _chai.expect)(_context2.t0.name).to.equal('ValidationError');
-
-                        case 11:
-                        case 'end':
-                            return _context2.stop();
-                    }
-                }
-            }, _callee2, this, [[0, 7]]);
-        }));
-
-        it('should fail with invalid sources', _regenerator2.default.mark(function _callee3() {
-            return _regenerator2.default.wrap(function _callee3$(_context3) {
-                while (1) {
-                    switch (_context3.prev = _context3.next) {
-                        case 0:
-                            _context3.prev = 0;
-
-                            this._testData.source.sources.push({ lang: {} });
-                            _context3.next = 4;
-                            return _DefinitionModel.DefinitionModel.checkValidity(this._testData.source);
-
-                        case 4:
-                            _assert2.default.fail('should fail');
-                            _context3.next = 11;
-                            break;
-
-                        case 7:
-                            _context3.prev = 7;
-                            _context3.t0 = _context3['catch'](0);
-
-                            (0, _chai.expect)(_context3.t0.errors).to.have.property('sources.1.lang');
-                            (0, _chai.expect)(_context3.t0.name).to.equal('ValidationError');
-
-                        case 11:
-                        case 'end':
-                            return _context3.stop();
-                    }
-                }
-            }, _callee3, this, [[0, 7]]);
-        }));
+        it('should fail with invalid sources', function* () {
+            try {
+                this._testData.source.sources.push({lang: {}});
+                yield DefinitionModel.checkValidity(this._testData.source);
+                assert.fail('should fail');
+            } catch (err) {
+                expect(err.errors).to.have.property('sources.1.lang');
+                expect(err.name).to.equal('ValidationError');
+            }
+        });
     });
 });
--- server/test/word/service/DefinitionService.test.js
+++ server/test/word/service/DefinitionService.test.js
@@ -1,47 +1,15 @@
 'use strict';
-
-var _regenerator = require('babel-runtime/regenerator');
-
-var _regenerator2 = _interopRequireDefault(_regenerator);
-
-var _sinon = require('sinon');
-
-var _sinon2 = _interopRequireDefault(_sinon);
-
-var _assert = require('assert');
-
-var _assert2 = _interopRequireDefault(_assert);
-
-var _chai = require('chai');
-
-var _stub = require('../stubs/stub.js');
-
-var stubs = _interopRequireWildcard(_stub);
-
-var _DefinitionService = require('../../../src/definition/servirce/DefinitionService.js');
-
-var _DefinitionModel = require('../../../src/definition/model/DefinitionModel.js');
-
-function _interopRequireWildcard(obj) {
-    if (obj && obj.__esModule) {
-        return obj;
-    } else {
-        var newObj = {};if (obj != null) {
-            for (var key in obj) {
-                if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key];
-            }
-        }newObj.default = obj;return newObj;
-    }
-}
-
-function _interopRequireDefault(obj) {
-    return obj && obj.__esModule ? obj : { default: obj };
-}
+import sinon from 'sinon';
+import assert from 'assert';
+import { expect } from 'chai';
+import * as stubs from '../stubs/stub.js';
+import { definitionService } from '../../../src/definition/servirce/DefinitionService.js';
+import { DefinitionModel } from '../../../src/definition/model/DefinitionModel.js';
 
 describe('Definition Service', function () {
     beforeEach(function () {
-        this.sandbox = _sinon2.default.sandbox.create();
-        this.__testData = { source: stubs.createStubWord() };
+        this.sandbox = sinon.sandbox.create();
+        this.__testData = {source: stubs.createStubWord()};
     });
 
     afterEach(function () {
@@ -50,60 +18,28 @@ describe('Definition Service', function () {
 
     describe('insert', function () {
 
-        it('should handle success', _regenerator2.default.mark(function _callee() {
-            var response, spy, addDefinitionResponse;
-            return _regenerator2.default.wrap(function _callee$(_context) {
-                while (1) {
-                    switch (_context.prev = _context.next) {
-                        case 0:
-                            response = {};
+        it('should handle success', function* () {
+            let response = {};
+            this.sandbox.stub(DefinitionModel, 'createDefinition', stubs.resolveWith(response));
+            let spy = this.sandbox.spy();
+            definitionService.on('definitionCreated', spy);
+            let addDefinitionResponse = yield definitionService.addDefinition(this.__testData.source);
 
-                            this.sandbox.stub(_DefinitionModel.DefinitionModel, 'createDefinition', stubs.resolveWith(response));
-                            spy = this.sandbox.spy();
+            expect(addDefinitionResponse).to.equal(response);
+            expect(spy.called).to.equal(true);
+        });
 
-                            _DefinitionService.definitionService.on('definitionCreated', spy);
-                            _context.next = 6;
-                            return _DefinitionService.definitionService.addDefinition(this.__testData.source);
-
-                        case 6:
-                            addDefinitionResponse = _context.sent;
-
-                            (0, _chai.expect)(addDefinitionResponse).to.equal(response);
-                            (0, _chai.expect)(spy.called).to.equal(true);
-
-                        case 9:
-                        case 'end':
-                            return _context.stop();
-                    }
-                }
-            }, _callee, this);
-        }));
-
-        it('should handle failure', _regenerator2.default.mark(function _callee2() {
-            var error, spy;
-            return _regenerator2.default.wrap(function _callee2$(_context2) {
-                while (1) {
-                    switch (_context2.prev = _context2.next) {
-                        case 0:
-                            error = {};
-
-                            this.sandbox.stub(_DefinitionModel.DefinitionModel, 'createDefinition', stubs.rejectWith(error));
-                            spy = this.sandbox.spy();
-
-                            _DefinitionService.definitionService.on('definitionCreated', spy);
-                            try {
-                                _DefinitionService.definitionService.addDefinition(this.__testData.source);
-                            } catch (err) {
-                                (0, _chai.expect)(err).to.equal(error);
-                                (0, _chai.expect)(spy.called).to.equal(false);
-                            }
-
-                        case 5:
-                        case 'end':
-                            return _context2.stop();
-                    }
-                }
-            }, _callee2, this);
-        }));
+        it('should handle failure', function* () {
+            let error = {};
+            this.sandbox.stub(DefinitionModel, 'createDefinition', stubs.rejectWith(error));
+            let spy = this.sandbox.spy();
+            definitionService.on('definitionCreated', spy);
+            try {
+                definitionService.addDefinition(this.__testData.source);
+            } catch(err) {
+                expect(err).to.equal(error);
+                expect(spy.called).to.equal(false);
+            }
+        });
     });
 });
